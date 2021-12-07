c39a007a001ffce074f957aaa2f39c0729b590ed
vadim123544@gmail.com <vadim123544@gmail.com>
2021-12-07 17:05:43 +0200

    Scaner done

--- readme.txt
+++ readme.txt
@@ -1,3 +1,11 @@
 This is a scanner of network
-Version 0
+
+Steps to create a network scanner is next:
+ - import a network library SCAPY, who can work with network protocols
+ - use a ARP function from this library to ask who has target IP (create a object of ARP request)
+ - Set destination MAC to broadcast MAC (create a object of broadcast frame)
+ - combine together this two objects, and receive one object with all these parameters
+ - then we have parse a response and extract a useful info in loop
+ 
+ 
 
--- scanner.py
+++ scanner.py
@@ -1,3 +1,32 @@
 #This is a network  scanner
 
+import scapy.all as scapy
 
+def scan(ip):
+    #Create a ARP frame
+    arp_request = scapy.ARP(pdst=ip)
+
+    #Create ethernet frame
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    #Create a combining object
+    arp_request_broadcast = broadcast/arp_request
+
+    #Send packet to network and parse a response (timeout we need
+    # because if we dont have response  programm must be continue)
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
+
+    #list_item = []
+
+    file = open('scan.txt', 'w')
+
+    for element in answered_list:
+            # Print a header of our loop
+            file.write("IP\t\t\tMACAddress\n")
+            file.write(element[1].psrc + "\t\t" + element[1].hwsrc)
+
+    #result_list = [list_header, list_item]
+    file.close()
+
+if __name__ == '__main__':
+    scan("10.6.2.1/24")
